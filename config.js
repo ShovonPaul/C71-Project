90d90a5afdcb836836af8357f4cc9cf70b46d29e
ShovonPaul <110727330+ShovonPaul@users.noreply.github.com>
2023-04-02 12:47:56 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyAgcrhbznJ1LVeVDPHUfMkFdu0f5eEZ13Q",
+    authDomain: "e-rideapp-5a67e.firebaseapp.com",
+    projectId: "e-rideapp-5a67e",
+    storageBucket: "e-rideapp-5a67e.appspot.com",
+    messagingSenderId: "1096637423248",
+    appId: "1:1096637423248:web:d6928ce8d8fcdce44b8215"
+  };
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
